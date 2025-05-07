68efd86f4bc1186848e9306e55d28706144d7f9d
joeglantern <your.email@example.com>
2025-05-07 23:39:40 +0300

    Fix Next.js configuration for Netlify deployment

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Uv9kTXHu+FrHvQdv8bxz8InIQlZFD0KvYZVZMBmukdE=\"\n}"
